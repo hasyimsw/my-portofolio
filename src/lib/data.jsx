6104314ae6c2d9d8da28ab8043824e7f3b69c053
hasyimsw_ <hasyimsriewahyudi@gmail.com>
2026-01-19 14:56:53 +0700

    update file

--- src/lib/data.jsx
+++ src/lib/data.jsx
@@ -36,7 +36,7 @@ export const projectsData = [
     title: "CorpComment",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    tags: ["Vite", "Tailwind css", "framer motion", "JS", "HTML"],
     imageUrl: corpcommentImg,
   },
   {
